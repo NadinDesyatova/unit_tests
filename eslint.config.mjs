5c1b136f7823eb9cc3fbf7ade7a7596492cba2ca
NadinDesyatova <nadindinina@yandex.ru>
2024-10-08 09:52:02 +0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+export default [
+  {
+    ignores: [
+      ".dist/",
+      ".coverage/",
+      ".docs/"
+    ]
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Character from './js/character.js';
+
+
+const character = new Character('Маг',90);
+character.health_status();
